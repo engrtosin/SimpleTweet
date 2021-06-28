f3ce421ed009ef9bf0729393c17230e6ead94cf0
Oluwatosin Oluwamayowa Ajibola <oluwatosinaj@fb.com>
2021-06-28 17:14:17 -0400

    Created the Tweet and User models.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -27,12 +27,12 @@ public class TimelineActivity extends AppCompatActivity {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.i(TAG,"onSuccess");
+                Log.i(TAG,"onSuccess " + json.toString());
             }
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.e(TAG,"onFailure", throwable);
+                Log.e(TAG,"onFailure " + response, throwable);
             }
         });
     }
